013ec2358b178fa8628ef68cdd94769df693356d
Gabriel Pereira Matuk <gabriel.matuk@eteg.com.br>
2021-08-12 19:02:39 -0300

    first commit

---
+++ queueAndStack.py
@@ -0,0 +1,49 @@
+#Nome: Gabriel Pereira Matuk
+#Turma: BSI - Bacharelado em Sistema de Informação
+#Data: 12/08/2021
+
+pessoasFila = ['gabriel','marcos','claudio','rebeca','marcio']
+print("Fila inicial")
+print(pessoasFila)
+
+pessoasFila.append('Claudia')
+print(pessoasFila)
+pessoasFila.append('Larissa')
+print(pessoasFila)
+pessoasFila.append('Marisa')
+print(pessoasFila)
+pessoasFila.append('Maria')
+print(pessoasFila)
+
+pessoasFila.pop(0)
+print(pessoasFila)
+pessoasFila.pop(0)
+print(pessoasFila)
+pessoasFila.pop(0)
+print(pessoasFila)
+pessoasFila.pop(0)
+print(pessoasFila)
+
+print("========================================================================")
+
+pessoasPilha = ['gabriel','marcos','claudio','rebeca','marcio']
+print("Pilha inicial")
+print(pessoasPilha)
+
+pessoasPilha.append('Claudia')
+print(pessoasPilha)
+pessoasPilha.append('Larissa')
+print(pessoasPilha)
+pessoasPilha.append('Marisa')
+print(pessoasPilha)
+pessoasPilha.append('Maria')
+print(pessoasPilha)
+
+pessoasPilha.pop()
+print(pessoasPilha)
+pessoasPilha.pop()
+print(pessoasPilha)
+pessoasPilha.pop()
+print(pessoasPilha)
+pessoasPilha.pop()
+print(pessoasPilha)
